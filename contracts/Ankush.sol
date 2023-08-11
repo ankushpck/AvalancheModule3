eea6aaeb11a475f60266a4829da7915d7203df1e
ankushpck <138556460+ankushpck@users.noreply.github.com>
2023-08-11 19:04:11 +0530

    Ankush.sol

--- contracts/Ankush.sol
+++ contracts/Ankush.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Ranjan is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Ranjan", "Rjn") {}
+contract Ankush is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("ankush", "akh") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
